68d17bf04c00cc21233c3effe602cbe18d37e1ee
Asside333 <baptiste.pardon@gmail.com>
2026-03-29 05:20:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache offline, stratégie Cache-First
    ================================================================ */
 
-const CACHE_NAME = 'matrice-v30';
+const CACHE_NAME = 'matrice-v31';
 
 // Fichiers à mettre en cache à l'installation
 const ASSETS_TO_CACHE = [
